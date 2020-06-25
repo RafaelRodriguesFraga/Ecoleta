361769eda5ff4f293a174337c6cf9b221de6edff
Rafael Fraga <ti.rafaelfraga@gmail.com>
2020-06-25 09:42:37 -0300

    Adiciona validação de campos

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,9 @@
-import express from 'express';
-import PointsController from './controllers/PointsController';
-import ItensController from './controllers/ItensController';
-import multer from 'multer';
-import multerConfig from './config/multer';
+import express from "express";
+import PointsController from "./controllers/PointsController";
+import ItensController from "./controllers/ItensController";
+import multer from "multer";
+import multerConfig from "./config/multer";
+import { celebrate, Joi } from "celebrate";
 
 const routes = express.Router();
 const upload = multer(multerConfig);
@@ -12,7 +13,29 @@ const itensController = new ItensController();
 
 routes.get("/itens", itensController.index);
 
-routes.post("/points", upload.single('image'), pointsController.create);
+routes.post(
+  "/points",
+  upload.single("image"),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        itens: Joi.string().required(),
+      }),
+    },
+    {
+      abortEarly: false,
+    }
+  ),
+  pointsController.create
+);
+
 routes.get("/points/:id", pointsController.show);
 routes.get("/points", pointsController.index);
 
